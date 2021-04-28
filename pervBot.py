e2248fa25b6be9538124a65431bc8f0fc1ad2a70
Jet Simon <jetjdsimon@gmail.com>
2021-04-27 23:21:59 -0700

    add custom text and meme support to images lol

--- pervBot.py
+++ pervBot.py
@@ -3,7 +3,7 @@ from pervTools import *
 from dotenv import load_dotenv
 import fitbit
 
-
+USE_FITBIT = True
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,10 +15,23 @@ CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 REFRESH_TOKEN = os.getenv('REFRESH_TOKEN')
 
-authd_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+if USE_FITBIT:
+    authd_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
 
 
 client = discord.Client()
+
+def BPM():
+    if USE_FITBIT:
+        #try to add heart BPM text and if not then forget about it
+        try:
+            heart_data = authd_client.intraday_time_series('activities/heart', base_date=get_right_dateFormat(), detail_level='1sec')['activities-heart-intraday']['dataset'][-1]
+            BPM = str(heart_data['value'])
+            BPM_TEXT = BPM + " BPM (" + heart_data['time'] + ")"
+            return BPM_TEXT
+        except fitbit.exceptions.HTTPTooManyRequests:
+            print("HEART DATA NOT AVAILABLE (TOO MANY REQUESTS)")
+    return None
 
 @client.event
 async def on_ready():
@@ -37,18 +50,31 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content == '!perv':
-        try:
-            heart_data = authd_client.intraday_time_series('activities/heart', base_date=get_right_dateFormat(), detail_level='1sec')['activities-heart-intraday']['dataset'][-1]
-            BPM = str(heart_data['value'])
-            BPM_TEXT = "Jet's heart was last measured beating at " + BPM + " BPM (" + heart_data['time'] + ")"
-        except fitbit.exceptions.HTTPTooManyRequests:
-            BPM_TEXT = "HEART DATA NOT AVAILABLE (TOO MANY REQUESTS)"
-        
-        path = takePic()
+    if message.content[:5] == '!perv':
+
+        text = BPM() #defaults to None if not using BPM or error, don't worry
+            
+        if len(message.content.split(" ", 1)) > 1:
+            text = message.content.split(" ",1)[1]
+
+        #creates pic and adds text if exists
+        path = takePic(text)
         picture = discord.File(path)
-        await message.channel.send(BPM_TEXT,file=picture)
-        os.remove(path)
+        await message.channel.send(file=picture)
+        os.remove(path) #delete pic after so jets computer doesnt crash
+
+    elif message.content[:5] == '!meme':
+        if len(message.content.split(" ", 1)) > 1 and ":" in message.content:
+            text = message.content.split(" ",1)[1]
+            top = text.split(":")[0]
+            bottom = text.split(":")[1]
+            #creates pic and adds text if exists
+            path = makeMeme(top, bottom)
+            picture = discord.File(path)
+            await message.channel.send(file=picture)
+            os.remove(path) #delete pic after so jets computer doesnt crash
+        else:
+            await message.channel.send("MEME MUST BE IN FORMAT !meme [top]:[bottom]")
 
 
 client.run(TOKEN)
--- pervTools.py
+++ pervTools.py
@@ -1,21 +1,58 @@
 import cv2
 from random import randint
-import fitbit
 import datetime
 
-def takePic():
+def takePic(text = None):
     video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     # Check success
     if not video_capture.isOpened():
         raise Exception("Could not open video device")
     # Read picture. ret === True on success
     ret, frame = video_capture.read()
+    
+    #Text adding stuff
+    if text:
+        h,w,c = frame.shape
+        font = cv2.FONT_HERSHEY_TRIPLEX
+        processedText = text.split("%n")
+
+        lh = 25
+        
+        for line in processedText:
+            cv2.putText(frame,line,(10,lh), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+            lh+=25
+
+    # Save file to computer w random name
     path = str(randint(0,10000)) + ".jpg"
     cv2.imwrite(path, frame)
+
     # Close device
     video_capture.release()
     cv2.destroyAllWindows()
-    return path
+    return path #return path for discord to load from
+
+def makeMeme(top, bottom):
+    video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    # Check success
+    if not video_capture.isOpened():
+        raise Exception("Could not open video device")
+    # Read picture. ret === True on success
+    ret, frame = video_capture.read()
+
+    h,w,c = frame.shape
+    font = cv2.FONT_HERSHEY_TRIPLEX
+
+    cv2.putText(frame,top,(int(w/4),25), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    cv2.putText(frame,bottom,(int(w/4),h-25), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+    # Save file to computer w random name
+    path = str(randint(0,10000)) + ".jpg"
+    cv2.imwrite(path, frame)
+
+    # Close device
+    video_capture.release()
+    cv2.destroyAllWindows()
+    return path #return path for discord to load from
 
 #https://github.com/thegabriele97/AmiRunning-code/blob/2ef3732a30eb12c3ad12e35c9f13ae28bcab8559/src/RaspberryPi/fitbitPackage/fitbit_api.py#L31
 def get_right_dateFormat(offset: int = 0) -> str:
